02b9f1e9828e7b79151b5f4b40825ba4e7717a1f
zlurker <tallspin_97@hotmail.com>
2016-07-17 22:06:37 +0800

    Reupload

---
+++ MusicCollide/Assets/Scripts/LevelElementsScripts/RotationScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
